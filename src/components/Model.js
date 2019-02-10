a7c32680ae3cd18b0b57241e2071d781fbe1629f
tokyosuite <helenyueyu@gmail.com>
2019-02-10 12:29:55 -0800

    Successfully created the Uno deck. Tests all pass.

--- src/components/Model.js
+++ src/components/Model.js
@@ -2,7 +2,48 @@ const types = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "null", "reverse", "draw"]
 
 const colors = ["red", "yellow", "green", "blue"]
 
+
+
+
+const createMainDeck = () => {
+  const array = []
+  const wild = {
+    type: "wild",
+    color: "none"
+  }
+  const wild4 = {
+    type: "wild4",
+    color: "none"
+  }
+
+  for (let x in types) {
+    for (let y in colors) {
+      if (types[x] === 0) {
+        array.push({
+          type: types[x],
+          color: colors[y]
+        })
+      } else {
+        array.push({
+          type: types[x],
+          color: colors[y]
+        }, {
+          type: types[x],
+          color: colors[y]
+        })
+      }
+    }
+  }
+  for (let i = 0; i < 4; i++) {
+    array.push(wild)
+    array.push(wild4)
+  }
+  return array
+}
+
+
 module.exports = {
   types: types,
-  colors: colors
+  colors: colors,
+  createMainDeck: createMainDeck
 }
--- src/components/Model.test.js
+++ src/components/Model.test.js
@@ -10,6 +10,27 @@ test('The total number of card colors is 4.', () => {
   expect(model.colors.length).toBe(4)
 })
 
+test('The deck contains exactly 4 zeroes.', () => {
+  expect(model.createMainDeck().filter(x => x.type===0).length).toBe(4)
+})
+
+test('The deck contains exactly 8 cards of each other number, ranging from 1 to 9.', () => {
+  for (let i = 1; i < 10; i++) {
+    expect(model.createMainDeck().filter(x => x.type===i).length).toBe(8)
+  }
+})
+
+test('Expect the deck to contain 8 of each: null, reverse, draw.', () => {
+  expect(model.createMainDeck().filter(x => x.type==="null").length).toBe(8)
+  expect(model.createMainDeck().filter(x => x.type==="reverse").length).toBe(8)
+  expect(model.createMainDeck().filter(x => x.type==="draw").length).toBe(8)
+})
+
+test('Expect the deck to contain 4 of each type of wild card', () => {
+  expect(model.createMainDeck().filter(x => x.type==="wild").length).toBe(4)
+  expect(model.createMainDeck().filter(x => x.type==="wild4").length).toBe(4)
+})
+
 // describe('Array', function() {
 //   describe('#indexOf()', function() {
 //     it('should return -1 when the value is not present', function() {
